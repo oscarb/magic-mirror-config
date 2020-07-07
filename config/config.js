5da8dfff5685687c58dfd090c02016f649739294
Oscar Björkman <git@oscarb.se>
2020-07-07 22:03:09 +0200

    Remove weather (#5)

--- config/config.js
+++ config/config.js
@@ -9,82 +9,78 @@
  */
 
  /* Secrets from URL params */
-const urlParams = new URLSearchParams(location.search);
-const secrets = Object.fromEntries(urlParams);
+ const secrets = (typeof location !== "undefined") ? Object.fromEntries(new URLSearchParams(location.search)) : {}; 
 
-var config = {
-	address: "0.0.0.0", // Address to listen on, can be:
-	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-	                      // - another specific IPv4/6 to listen on a specific interface
-	                      // - "", "0.0.0.0", "::" to listen on any interface
-	                      // Default, when address config is left out, is "localhost"
-	port: 8080,
-	ipWhitelist: [], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
-
-	language: "sv",
-	timeFormat: 24,
-	units: "metric",
-	customCss: "css/custom.css",
-
-	modules: [
-		{
-			module: "alert",
-		},
-		{
-			module: "updatenotification",
-			position: "top_bar"
-		},
-		{
-			module: "clock",
-			position: "top_left",
-			config: {
-				displaySeconds: false,
-				dateFormat: "dddd D MMMM[<br><small>vecka ]W[</small>]",
-			}
-		},
-		{
-			module: "compliments",
-			position: "lower_third",
-			config: {
-				compliments: {
-					anytime: [
-						"Hey there " + secrets.apiKey
-					],
-					morning: [
-						"Hej!"
-					],
-					afternoon: [
-						"Hej"
-					],
-					evening: [
-						"Hej"
-					]
-				}
-			}
-		},
-		{
-			module: "newsfeed",
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true
-			}
-		},
-	]
-
-};
-
-/*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
-
-
+ var config = {
+	 address: "0.0.0.0", // Address to listen on, can be:
+						   // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+						   // - another specific IPv4/6 to listen on a specific interface
+						   // - "", "0.0.0.0", "::" to listen on any interface
+						   // Default, when address config is left out, is "localhost"
+	 port: 8080,
+	 ipWhitelist: [], // Set [] to allow all IP addresses
+															// or add a specific IPv4 of 192.168.1.5 :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+ 
+	 language: "sv",
+	 timeFormat: 24,
+	 units: "metric",
+	 customCss: "css/custom.css",
+ 
+	 modules: [
+		 {
+			 module: "alert",
+		 },
+		 {
+			 module: "clock",
+			 position: "top_left",
+			 config: {
+				 displaySeconds: false,
+				 dateFormat: "dddd D MMMM[<br><small>vecka ]W[</small>]",
+			 }
+		 },
+		 {
+			 module: "compliments",
+			 position: "lower_third",
+			 config: {
+				 compliments: {
+					 anytime: [
+						 "Hey there Oscar"
+					 ],
+					 morning: [
+						 "Hej!"
+					 ],
+					 afternoon: [
+						 "Hej"
+					 ],
+					 evening: [
+						 "Hej"
+					 ]
+				 }
+			 }
+		 },
+		 {
+			 module: "newsfeed",
+			 position: "bottom_bar",
+			 config: {
+				 feeds: [
+					 {
+						 title: "New York Times",
+						 url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					 }
+				 ],
+				 showSourceTitle: true,
+				 showPublishDate: true
+			 }
+		 },
+	 ]
+ 
+ };
+ 
+ /*************** DO NOT EDIT THE LINE BELOW ***************/
+ if (typeof module !== "undefined") {module.exports = config;}
+ 
+ 
+ 
